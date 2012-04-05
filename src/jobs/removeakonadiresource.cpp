bf7de47403d1c7d5481282f6a62d5bf94c26892a
Alex Fiestas <afiestas@kde.org>
2012-04-05 23:57:41 +0200

    Added generic job for removing akonadi resource

---
+++ src/jobs/removeakonadiresource.cpp
@@ -0,0 +1,55 @@
+/*************************************************************************************
+ *  Copyright (C) 2012 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include "removeakonadiresource.h"
+
+#include <QDebug>
+
+#include <akonadi/agenttype.h>
+#include <akonadi/agentmanager.h>
+#include <akonadi/agentinstancecreatejob.h>
+
+using namespace Akonadi;
+
+RemoveAkonadiResource::RemoveAkonadiResource(KConfigGroup& group, QObject* parent)
+: KJob(parent)
+, m_config(group)
+{
+
+}
+
+
+RemoveAkonadiResource::~RemoveAkonadiResource()
+{
+
+}
+
+void RemoveAkonadiResource::start()
+{
+    QMetaObject::invokeMethod(this, "removeResource", Qt::QueuedConnection);
+}
+
+void RemoveAkonadiResource::removeResource()
+{
+    QString id = m_config.group("private").readEntry("calendarAndTasksResource");
+    id.remove("org.freedesktop.Akonadi.Resource.");
+
+    AgentInstance instance = AgentManager::self()->instance(id);
+
+    AgentManager::self()->removeInstance(instance);
+}
