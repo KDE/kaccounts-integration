599bf3792f4d28b6596b6e65cfd3ea0ed9fd1a96
Àlex Fiestas <afiestas@kde.org>
2013-03-14 18:42:29 +0100

    First code drop of Accounts KDED
    This KDED will be in charge of enabling/disabling KDE services such
    akonadi, perhaps Telapathy etc.
    
    One of the things it will do (for example) is to create a PlavesView
    when an ownCloud account is configured.
    
    ATM it kinda works for Akonadi though I'm pushing this so mck182 can do
    some testing.

---
+++ src/daemon/daemon.cpp
@@ -0,0 +1,129 @@
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+#include "daemon.h"
+
+#include <QtCore/QTimer>
+#include <QDBusConnection>
+#include <QDBusMessage>
+#include <QDBusPendingCall>
+
+#include <kdebug.h>
+#include <kdemacros.h>
+#include <KLocalizedString>
+#include <KActionCollection>
+#include <KPluginFactory>
+
+#include <Accounts/Manager>
+#include <Accounts/Service>
+#include <Accounts/AccountService>
+
+#include <Akonadi/AgentType>
+#include <Akonadi/AgentManager>
+#include <Akonadi/AgentInstanceCreateJob>
+
+K_PLUGIN_FACTORY(KScreenDaemonFactory, registerPlugin<AccountsDaemon>();)
+K_EXPORT_PLUGIN(KScreenDaemonFactory("accounts", "accounts"))
+
+using namespace Akonadi;
+
+AccountsDaemon::AccountsDaemon(QObject* parent, const QList< QVariant >& )
+ : KDEDModule(parent)
+ , m_manager(new Accounts::Manager(this))
+{
+    kDebug();
+    Accounts::Account *acc = 0;
+    Accounts::ServiceList services;
+    Accounts::AccountIdList accList = m_manager->accountList();
+    Q_FOREACH(const Accounts::AccountId &id, accList) {
+        acc = m_manager->account(id);
+        services = acc->services();
+        Q_FOREACH(const Accounts::Service &service, services) {
+            acc->selectService(service);
+        }
+        connect(acc, SIGNAL(enabledChanged(QString,bool)), SLOT(enabledChanged(QString,bool)));
+    }
+    connect(m_manager, SIGNAL(accountCreated(Accounts::AccountId)), SLOT(accountCreated(Accounts::AccountId)));
+}
+
+AccountsDaemon::~AccountsDaemon()
+{
+    delete m_manager;
+}
+
+void AccountsDaemon::accountCreated(const Accounts::AccountId& id)
+{
+    kDebug() << id;
+    Accounts::Account *acc = m_manager->account(id);
+    Accounts::ServiceList services = acc->enabledServices();
+
+    Q_FOREACH(const Accounts::Service &service, services) {
+        findResource(service.name());
+    }
+}
+
+void AccountsDaemon::enabledChanged(const QString& serviceName, bool enabled)
+{
+    kDebug();
+    findResource(serviceName);
+}
+
+void AccountsDaemon::findResource(const QString &serviceName)
+{
+    kDebug() << serviceName;
+    QString mime = "text/x-vnd.accounts.";
+    mime.append(serviceName);
+
+    AgentType::List types = AgentManager::self()->types();
+    Q_FOREACH(const AgentType &type, types) {
+        if (!type.mimeTypes().contains(mime)) {
+            continue;
+        }
+
+        createResource(type);
+    }
+}
+
+void AccountsDaemon::createResource(const AgentType& type)
+{
+    AgentInstanceCreateJob *job = new AgentInstanceCreateJob( type, this );
+    connect( job, SIGNAL(result(KJob*)), SLOT(resourceCreated(KJob*)));
+    job->start();
+}
+
+void AccountsDaemon::resourceCreated(KJob* job)
+{
+    kDebug();
+    if (job->error()) {
+        kDebug() << "Error creating the job";
+        return;
+    }
+
+    kDebug();
+    AgentInstance agent = qobject_cast<AgentInstanceCreateJob*>( job )->instance();
+    agent.setName("YAYAYA");
+    QString service = "org.freedesktop.Akonadi.Resource." + agent.identifier();
+    QString path = "/org/kde/mklapetek";
+    QString method = "org.kde.akonadi_";
+    method.append(agent.identifier());
+    method.append("MicroblogResource");
+
+    QDBusMessage msg = QDBusMessage::createMethodCall(service, path, method, "configureByAccount");
+
+    QDBusPendingCall call = QDBusConnection::sessionBus().asyncCall(msg);
+    call.waitForFinished();
+}
---
+++ src/daemon/daemon.h
@@ -0,0 +1,56 @@
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#ifndef ACCOUNTS_DAEMON_H
+#define ACCOUNTS_DAEMON_H
+
+#include <kdedmodule.h>
+
+#include <Accounts/Account>
+
+namespace Accounts {
+    class Manager;
+};
+
+namespace Akonadi {
+    class AgentType;
+};
+class KJob;
+
+class KDE_EXPORT AccountsDaemon : public KDEDModule
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.Accounts")
+
+    public:
+        AccountsDaemon(QObject *parent, const QList<QVariant>&);
+        virtual ~AccountsDaemon();
+
+    public Q_SLOTS:
+        void accountCreated(const Accounts::AccountId &id);
+        void resourceCreated(KJob* job);
+        void enabledChanged(const QString &serviceName, bool enabled);
+
+    private:
+        void findResource(const QString &serviceName);
+        void createResource(const Akonadi::AgentType &type);
+
+        Accounts::Manager* m_manager;
+};
+
+#endif /*KSCREN_DAEMON_H*/
