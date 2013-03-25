1c1653578221ed33d98ec49fd20ca05d8753a673
Àlex Fiestas <afiestas@kde.org>
2013-03-25 16:10:07 +0100

    Add FetchSettingsJob with unittest
    Basically this small job will allow us to fetch any setting from the
    exported resource Settings.

---
+++ src/daemon/fetchsettingsjob.cpp
@@ -0,0 +1,130 @@
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include "fetchsettingsjob.h"
+#include "dbussettingspathjob.h"
+
+#include <QDBusMessage>
+#include <QDBusConnection>
+#include <QDBusInterface>
+#include <QDBusPendingCall>
+#include <QDBusPendingReply>
+#include <QDBusPendingCallWatcher>
+#include <QtXml/QDomDocument>
+
+#include <kdebug.h>
+
+FetchSettingsJob::FetchSettingsJob(QObject* parent)
+ : KJob(parent)
+ , m_watcher(0)
+{
+
+}
+
+FetchSettingsJob::~FetchSettingsJob()
+{
+    delete m_watcher;
+}
+
+void FetchSettingsJob::start()
+{
+    kDebug();
+    QMetaObject::invokeMethod(this, "init", Qt::QueuedConnection);
+}
+
+void FetchSettingsJob::init()
+{
+    kDebug();
+    if (m_interface.isEmpty()) {
+        Q_ASSERT_X(!m_resourceId.isEmpty(), "Asking for the dbus itnerface", "Resource id can't be empty");
+        DBusSettingsPathJob *job = new DBusSettingsPathJob(this);
+        connect(job, SIGNAL(finished(KJob*)), SLOT(dbusSettingsPathDone(KJob*)));
+        job->setResourceId(m_resourceId);
+        job->start();
+        return;
+    }
+
+    fetchSettings();
+}
+
+void FetchSettingsJob::dbusSettingsPathDone(KJob* job)
+{
+    if (job->error()) {
+        setError(job->error());
+        setErrorText(job->errorText());
+        emitResult();
+        return;
+    }
+
+    DBusSettingsPathJob *dbusJob = qobject_cast<DBusSettingsPathJob*>(job);
+    m_interface = dbusJob->interface();
+
+    fetchSettings();
+}
+
+void FetchSettingsJob::fetchSettings()
+{
+    Q_ASSERT_X(!m_interface.isEmpty(), "Fetching settings", "We need Interface to be set");
+    Q_ASSERT_X(!m_resourceId.isEmpty(), "Fetching settings", "We need resource Id to be set");
+    Q_ASSERT_X(!m_key.isEmpty(), "Fetching settings", "We need the key to be set");
+
+    QString service = "org.freedesktop.Akonadi.Resource." + m_resourceId;
+    QString path = "/Settings";
+
+    QDBusMessage msg = QDBusMessage::createMethodCall(service, path, m_interface, m_key);
+
+    QDBusInterface resource(service, path, m_interface);
+    QDBusPendingCall reply = resource.asyncCall(m_key);
+
+    m_watcher = new QDBusPendingCallWatcher(reply, this);
+    connect(m_watcher, SIGNAL(finished(QDBusPendingCallWatcher*)), SLOT(fetchDone(QDBusPendingCallWatcher*)));
+}
+
+void FetchSettingsJob::fetchDone(QDBusPendingCallWatcher* watcher)
+{
+    if (watcher->isError()) {
+        setError(-1);
+    }
+
+    emitResult();
+}
+
+QString FetchSettingsJob::key() const
+{
+    return m_key;
+}
+
+void FetchSettingsJob::setKey(const QString& key)
+{
+    m_key = key;
+}
+
+QString FetchSettingsJob::interface() const
+{
+    return m_interface;
+}
+
+void FetchSettingsJob::setInterface(const QString& interface)
+{
+    m_interface = interface;
+}
+
+void FetchSettingsJob::setResourceId(const QString& resourceId)
+{
+    m_resourceId = resourceId;
+}
---
+++ tests/testfetchsettingsjob.cpp
@@ -0,0 +1,103 @@
+/*************************************************************************************
+ *  Copyright (C) 2013 by Alejandro Fiestas Olivares <afiestas@kde.org>              *
+ *                                                                                   *
+ *  This program is free software; you can redistribute it and/or                    *
+ *  modify it under the terms of the GNU General Public License                      *
+ *  as published by the Free Software Foundation; either version 2                   *
+ *  of the License, or (at your option) any later version.                           *
+ *                                                                                   *
+ *  This program is distributed in the hope that it will be useful,                  *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of                   *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    *
+ *  GNU General Public License for more details.                                     *
+ *                                                                                   *
+ *  You should have received a copy of the GNU General Public License                *
+ *  along with this program; if not, write to the Free Software                      *
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA   *
+ *************************************************************************************/
+
+#include <QtTest/QtTest>
+
+#include "../src/daemon/fetchsettingsjob.h"
+
+#include <QDBusConnection>
+#include <QDBusAbstractAdaptor>
+#include <unistd.h>
+
+class FakeAkonadiResource : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.Akonadi.fakeResource.Settings")
+
+public:
+    explicit FakeAkonadiResource(QObject* parent = 0);
+
+public Q_SLOTS:
+    int accountId();
+    void setAccountId(int accountId);
+
+private:
+    int m_accountId;
+};
+
+FakeAkonadiResource::FakeAkonadiResource(QObject* parent) : QDBusAbstractAdaptor(parent)
+{
+    m_accountId = 42;
+}
+
+int FakeAkonadiResource::accountId()
+{
+    return m_accountId;
+}
+
+void FakeAkonadiResource::setAccountId(int accountId)
+{
+    qDebug() << "AccId: " << accountId;
+}
+
+class testFetchSettingsJob : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit testFetchSettingsJob(QObject* parent = 0);
+
+private Q_SLOTS:
+    void testChangeSettings();
+};
+
+testFetchSettingsJob::testFetchSettingsJob(QObject* parent): QObject(parent)
+{
+    QString path = "/Settings";
+
+    QDBusConnection conn = QDBusConnection::sessionBus();
+    conn.registerService("org.freedesktop.Akonadi.Agent.akonadi_fake_resource_116");
+    conn.registerService("org.freedesktop.Akonadi.Resource.akonadi_fake_resource_116");
+    conn.registerObject(path, new FakeAkonadiResource(qApp), QDBusConnection::ExportAllContents);
+}
+
+void testFetchSettingsJob::testChangeSettings()
+{
+    FetchSettingsJob *job = new FetchSettingsJob(this);
+
+    job->setResourceId("akonadi_fake_resource_116");
+    job->setKey("accountId");
+    job->exec();
+
+    QVERIFY2(!job->error(), "Job is set as finished with error");
+    QCOMPARE(job->value<int>(), 42);
+
+    job = new FetchSettingsJob(this);
+
+    job->setResourceId("akonadi_fake_resource_116");
+    job->setKey("accountId");
+    job->setInterface("org.kde.Akonadi.fakeResource.Settings");
+    job->exec();
+
+    QVERIFY2(!job->error(), "Job is set as finished with error");
+    QCOMPARE(job->value<int>(), 42);
+}
+
+QTEST_MAIN(testFetchSettingsJob)
+
+#include "testfetchsettingsjob.moc"
