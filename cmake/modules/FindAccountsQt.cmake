9501094e40db4fb7f036bcd8fad5e973b32eff43
Àlex Fiestas <afiestas@kde.org>
2013-06-07 18:28:54 +0200

    Make ACCOUNTSQT_FOUND and SIGNONQT_FOUND work

--- cmake/modules/FindAccountsQt.cmake
+++ cmake/modules/FindAccountsQt.cmake
@@ -4,14 +4,12 @@
 #  ACCOUNTSQT_INCLUDE_DIRS - The libaccounts-qt include directories
 #  ACCOUNTSQT_LIBRARIES - The libraries needed to use libaccounts-qt
 
-find_package(PkgConfig)
-
 find_path(ACCOUNTSQT_INCLUDE_DIR Accounts/Account
-          HINTS ${ACCOUNTSQT_INCLUDEDIR} ${ACCOUNTSQT_INCLUDE_DIRS} ${CMAKE_INSTALL_PREFIX}/include
+          HINTS ${CMAKE_INSTALL_PREFIX}/include
           PATH_SUFFIXES accounts-qt )
 
 find_library(ACCOUNTSQT_LIBRARY NAMES accounts-qt libaccounts-qt
-    HINTS ${ACCOUNTSQT_LIBDIR} ${ACCOUNTSQT_LIBRARY_DIRS} ${CMAKE_INSTALL_PREFIX}/lib64)
+    HINTS ${CMAKE_INSTALL_PREFIX}/lib64)
 
 set(ACCOUNTSQT_LIBRARIES ${ACCOUNTSQT_LIBRARY} )
 set(ACCOUNTSQT_INCLUDE_DIRS ${ACCOUNTSQT_INCLUDE_DIR} )
@@ -19,7 +17,7 @@ set(ACCOUNTSQT_INCLUDE_DIRS ${ACCOUNTSQT_INCLUDE_DIR} )
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set  ACCOUNTSQT_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(libaccounts-qt  DEFAULT_MSG
+find_package_handle_standard_args(accountsqt  DEFAULT_MSG
                                   ACCOUNTSQT_LIBRARY ACCOUNTSQT_INCLUDE_DIR)
 
 mark_as_advanced(ACCOUNTSQT_INCLUDE_DIR ACCOUNTSQT_LIBRARY )
--- cmake/modules/FindSignOnQt.cmake
+++ cmake/modules/FindSignOnQt.cmake
@@ -4,14 +4,9 @@
 #  SIGNONQT_INCLUDE_DIRS - The libsignon-qt include directories
 #  SIGNONQT_LIBRARIES - The libraries needed to use libsignon-qt
 
-find_package(PkgConfig)
+find_path(SIGNONQT_INCLUDE_DIR SignOn/AuthService PATH_SUFFIXES signon-qt )
 
-find_path(SIGNONQT_INCLUDE_DIR SignOn/AuthService
-          HINTS ${SIGNONQT_INCLUDEDIR} ${SIGNONQT_INCLUDE_DIRS}
-          PATH_SUFFIXES signon-qt )
-
-find_library(SIGNONQT_LIBRARY NAMES signon-qt libsignon-qt
-             HINTS ${SIGNONQT_LIBDIR} ${SIGNONQT_LIBRARY_DIRS} )
+find_library(SIGNONQT_LIBRARY NAMES signon-qt libsignon-qt)
 
 set(SIGNONQT_LIBRARIES ${SIGNONQT_LIBRARY} )
 set(SIGNONQT_INCLUDE_DIRS ${SIGNONQT_INCLUDE_DIR} )
@@ -19,7 +14,7 @@ set(SIGNONQT_INCLUDE_DIRS ${SIGNONQT_INCLUDE_DIR} )
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set  SIGNONQT_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(libsignon-qt  DEFAULT_MSG
+find_package_handle_standard_args(signonqt  DEFAULT_MSG
                                   SIGNONQT_LIBRARY SIGNONQT_INCLUDE_DIR)
 
 mark_as_advanced(SIGNONQT_INCLUDE_DIR SIGNONQT_LIBRARY )
