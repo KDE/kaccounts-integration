1dc4f0c9ca5d8725d4669eb501a6736e12c1e514
Martin Klapetek <mklapetek@kde.org>
2015-05-26 21:01:04 +0200

    Show dialogs on button clicked (not pressed)
    This adds back part of 47734ffd90bdd40350379ffecf7f055fb0eec9a8

--- src/accountwidget.cpp
+++ src/accountwidget.cpp
@@ -101,7 +101,7 @@ void AccountWidget::setAccount(Accounts::Account *account)
 
             QHBoxLayout *hlayout = new QHBoxLayout();
             QPushButton *imConfigButton = new QPushButton(i18n("Configure..."));
-            m_connections << connect(imConfigButton, &QPushButton::pressed, [=](){
+            m_connections << connect(imConfigButton, &QPushButton::clicked, [=](){
                 uiPlugin->init(KAccountsUiPlugin::ConfigureAccountDialog);
             });
 
