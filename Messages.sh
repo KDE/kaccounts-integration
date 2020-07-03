518491f5075db84f9f53375cd9a7ccc704d30186
Yuri Chornoivan <yurchor@ukr.net>
2020-07-03 08:59:18 +0300

    Fix message extraction

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,2 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui"` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp"` -o $podir/kaccounts-integration.pot
-rm -f rc.cpp
+$XGETTEXT `find . -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/kaccounts-integration.pot
